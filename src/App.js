6707480d54aac00b15aff11003d81c5e63517813
React-newb <115961250+React-newb@users.noreply.github.com>
2023-03-13 11:59:38 -0400

    Added bootstrap. Removed default logos and css.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+        <h1 className='text-center'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
